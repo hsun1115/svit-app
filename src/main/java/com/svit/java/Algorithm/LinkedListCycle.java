4073210a928737cac28ffb9b463a91c7b6bff7c1
hsun1115 <sunhao1115@gmail.com>
2019-02-28 10:40:26 -0800

    Add files via upload
    checkLoop

---
+++ src/main/java/com/svit/java/Algorithm/LinkedListCycle.java
@@ -0,0 +1,29 @@
+package com.svit.java.Algorithm;
+
+public class LinkedListCycle {
+	
+	 // Definition for singly-linked list.
+	  class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode(int x) {
+	          val = x;
+	          next = null;
+	      }
+	  }
+	public class Solution {
+	    public boolean hasCycle(ListNode head) {
+	        if(head ==null || head.next == null){
+	            return false;
+	        }
+	        ListNode slow = head;
+	        ListNode fast = head;
+	        while(fast.next!=null && fast.next.next!=null){
+	            slow=slow.next;
+	            fast = fast.next.next;
+	            if(fast==slow) return true;
+	        }
+	        return false;
+	    }
+	}
+}
